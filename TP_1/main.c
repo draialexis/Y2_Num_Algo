8c4866e869c379e011599385882f01f20c02439c
Alexis DRAI <drai.alexis@gmail.com>
2021-09-24 16:49:13 +0200

    Move into new repo

---
+++ TP_1/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+#include "matrices.h"
+#define N 5
+
+
+int main() {
+
+    srand(time(NULL));
+
+    int * matB = (int *)malloc(N * sizeof(int));
+    fillMatB(matB, N);
+
+    printf("\nMatrix B:\n");
+    printMatB(matB, N);
+
+    int * bord = (int *)malloc(N * N * sizeof(int));
+
+    fillBord(bord, N);
+
+    printf("\nBord:\n");
+    printIntMatrix(bord, N);
+//    free(bord);
+
+    double * dingDong = (double *)malloc(N * N * sizeof(double));
+
+    fillDingDong(dingDong, N);
+
+    printf("\nDing Dong:\n");
+    printDoubleMatrix(dingDong, N);
+//    free(dingDong);
+
+    return 0;
+}
---
+++ TP_1/matrices.h
@@ -0,0 +1,63 @@
+int i, j;
+
+void fillMatB(int * matB, int n){
+    for (i = 0; i < n; ++i) {
+        *(matB + i) = rand() % 20;
+    }
+}
+
+void printMatB(const int * matB, int n){
+    for (i = 0; i < n; i++) {
+        printf("%+04d\n", *(matB + i));
+    }
+}
+
+void printIntMatrix(const int * matrix, int n){
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%+04d ", *(matrix + i * n + j));
+        }
+        printf("\n");
+    }
+}
+
+void printDoubleMatrix(const double * matrix, int n){
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%+.2f ", *(matrix + i * n + j));
+        }
+        printf("\n");
+    }
+}
+
+void fillBord(int * bord, int n){
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n; ++j) {
+            if (i == j) {
+                *(bord + i * n + j) = 1;
+                /*
+                * address of a 'box': goes from address of first value (original pointer), and since
+                * the values are stored one after the other (in a line, more than in a square...),
+                * we add to it the number of rows that we've gone through, times the total number
+                * of columns, plus the number of columns that we've gone through.
+                * try it with an example (use i =/= j for helpful results)
+                */
+            } else if (i + 1 == n) {
+                *(bord + i * n + j) = (int)pow(2, (n - (j + 1)));
+            } else if (j + 1== n) {
+                *(bord + i * n + j) = (int)pow(2, (n - (i + 1)));
+            } else {
+                *(bord + i * n + j) = 0;
+            }
+        }
+    }
+}
+
+void fillDingDong(double * dingDong, int n){
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n; ++j) {
+            *(dingDong + i * n + j) = (double)1 / (2 * (n - (i + 1) - (j + 1) + 1.5));
+        }
+    }
+}
+
