0312946e01b649df270234814dfaa3382dd8d53d
Alexis DRAI <drai.alexis@gmail.com>
2021-09-25 17:44:57 +0200

    Improve readability of 2d matrix navigation code, add Cmake

--- TP_1/matrices.h
+++ TP_1/matrices.h
@@ -15,7 +15,7 @@ void printMatB(const int * matB, int n){
 void printIntMatrix(const int * matrix, int n){
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
-            printf("%+04d ", *(matrix + i * n + j));
+            printf("%+04d ", *(matrix + (i * n) + j));
         }
         printf("\n");
     }
@@ -24,7 +24,7 @@ void printIntMatrix(const int * matrix, int n){
 void printDoubleMatrix(const double * matrix, int n){
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
-            printf("%+.2f ", *(matrix + i * n + j));
+            printf("%+.2f ", *(matrix + (i * n) + j));
         }
         printf("\n");
     }
@@ -34,7 +34,7 @@ void fillBord(int * bord, int n){
     for (i = 0; i < n; ++i) {
         for (j = 0; j < n; ++j) {
             if (i == j) {
-                *(bord + i * n + j) = 1;
+                *(bord + (i * n) + j) = 1;
                 /*
                 * address of a 'box': goes from address of first value (original pointer), and since
                 * the values are stored one after the other (in a line, more than in a square...),
@@ -43,11 +43,11 @@ void fillBord(int * bord, int n){
                 * try it with an example (use i =/= j for helpful results)
                 */
             } else if (i + 1 == n) {
-                *(bord + i * n + j) = (int)pow(2, (n - (j + 1)));
+                *(bord + (i * n) + j) = (int)pow(2, (n - (j + 1)));
             } else if (j + 1== n) {
-                *(bord + i * n + j) = (int)pow(2, (n - (i + 1)));
+                *(bord + (i * n) + j) = (int)pow(2, (n - (i + 1)));
             } else {
-                *(bord + i * n + j) = 0;
+                *(bord + (i * n) + j) = 0;
             }
         }
     }
@@ -56,7 +56,7 @@ void fillBord(int * bord, int n){
 void fillDingDong(double * dingDong, int n){
     for (i = 0; i < n; ++i) {
         for (j = 0; j < n; ++j) {
-            *(dingDong + i * n + j) = (double)1 / (2 * (n - (i + 1) - (j + 1) + 1.5));
+            *(dingDong + (i * n) + j) = (double)1 / (2 * (n - (i + 1) - (j + 1) + 1.5));
         }
     }
 }
