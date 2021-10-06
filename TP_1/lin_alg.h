88746f74329ba61a46613d37d3c29dbebb0f1106
Alexis DRAI <drai.alexis@gmail.com>
2021-10-06 22:24:02 +0200

    Move functions around for clarity, add comments to indicate time complexity

--- TP_1/lin_alg.h
+++ TP_1/lin_alg.h
@@ -13,6 +13,107 @@ void gaussMethod(double **A, int rowsA, int colsA, double *B, int isJacobiHelper
 
 void solveGauss(double **A, int rowsA, int colsA, double *B, int homSys);
 
+// NB: this destructive function changes the matrices as a side-effect
+void gaussMethod(double **A, int rowsA, int colsA, double *B, int isJacobiHelper) {
+    if (A == NULL || rowsA < 2 || colsA < 2 || B == NULL) {
+        if (rowsA < 2 || colsA < 2) {
+            printf("we will not deal with a matA(m, n) if m or n is below 2\n");
+        }
+        EMPTY_OR_NULL
+        FAIL_OUT
+    }
+
+    printf("=======================\nGauss\n=======================\n");
+
+
+    int homSys = isHomSys(B, rowsA); //if B is nil, our job might just be made a lot easier
+
+    SHOWME
+    double factor;
+    for (int i = 0; i < rowsA; i++) {
+        int maxPos = i;
+        double maxVal = fabs(A[maxPos][i]);
+
+        for (int k = i + 1; k < rowsA; ++k) {
+            if (fabs(A[k][i]) - maxVal > EPSILON) { // swapping rows so that our leading coefficients are greater
+                maxVal = fabs(A[k][i]);
+                                // sum from i+1 to n : (n(n+1)/2) - 1 + (n(n+1)/2) - 2 + ... -> O(n²) calls to fabs()
+                maxPos = k;
+            }
+        }
+
+        if (maxPos != i) {
+            printf("L%d <-> L%d\n", i + 1, maxPos + 1);
+            rowSwap(A, B, i, maxPos, colsA);                                               //at worst 2n attributions
+            SHOWME
+        }
+
+        for (int k = i + 1; k < rowsA; ++k) {
+            if (fabs(A[k][i]) < EPSILON || fabs(A[i][i]) < EPSILON) {
+                //to make sure we don't handle numbers too small, for stability
+                continue;
+            } else {
+                factor = (A[k][i] / A[i][i]);
+                                //// at worst (n(n+1)/2) - 1 + (n(n+1)/2) - 2 + ... -> O(n^2) arithmetic operations
+                printf("L%d <- L%d - (%+06.1f * L%d)\n", k + 1, k + 1, factor, i + 1);
+                rowTransform(A[k], colsA, A[i], factor);
+                          //// at worst n * ((n(n+1)/2) - 1 + (n(n+1)/2) - 2 + ...) -> O(n^3) arithmetic operations
+                B[k] -= factor * B[i];
+                                //// at worst (n(n+1)/2) - 1 + (n(n+1)/2) - 2 + ... -> O(n^2) arithmetic operations
+                SHOWME
+            }
+        }
+    }
+    if (!isJacobiHelper) {
+        solveGauss(A, rowsA, colsA, B, homSys);                                //// -> O(n^2) arithmetic operations
+    }
+}
+
+// NB: this destructive function could change the matrices as a side-effect
+void solveGauss(double **A, int rowsA, int colsA, double *B, int homSys) {
+    if (A == NULL || B == NULL || rowsA <= 0 || colsA <= 0) {
+        printf("How on Earth?!\n");
+        EMPTY_OR_NULL
+        FAIL_OUT
+    }
+    int nilRowsCount = nilRows(A, rowsA, colsA, B, homSys);         //// at worst n^2 artihmetic operations -> O(n^2)
+    if (nilRowsCount == -1) {
+        return;
+    }
+
+    if (rowsA - nilRowsCount == colsA) { // deal with unique solution
+        printf("A * X = B a une solution\n");
+
+        if (homSys) {
+            printf("...le vecteur 0(%d)\n", colsA);
+        } else {
+            double *X = mkColVec(colsA);
+            X[colsA - 1] = B[colsA - 1] / A[colsA - 1][colsA - 1];
+            for (int i = colsA - 2; i >= 0; i--) { //we've already dealt with i = colsA - 1
+                double sum = 0;
+                for (int j = i + 1; j < colsA; j++) {
+                    sum += A[i][j] * X[j];
+                                 //// at worst (n(n+1)/2) - 1 + (n(n+1)/2) - 2 + ... -> O(n^2) arithmetic operations
+                }
+                X[i] = (B[i] - sum) / A[i][i];                          //// at worst n arithmetic operations -> O(n)
+            }
+            printf("X = \n");
+            showCol(X, colsA);
+            free(X);
+        }
+    } else if (rowsA - nilRowsCount < colsA) { // "deal" with infinite amount of solutions
+        // could probably just be phrased as "else"
+        printf("A * X = B a une infinite de solutions\n");
+        if (homSys) {
+            printf("...y compris le vecteur 0(%d)\n", colsA);
+        }
+    } else {
+        printf("we shouldn't be here... no solutions, right?\n");
+        DEBUG
+    }
+}
+
+// NB: this destructive function could change the matrices as a side-effect
 void jacobiMethod(double **A, int rowsA, int colsA, double *B, int maxIteration, double error) {
     if (A == NULL || rowsA < 2 || colsA < 2 || B == NULL || maxIteration < 1 || fabs(error) < EPSILON) {
         printf("les matrices doivent etre non-nulles, A doit etre au moins (2, 2), max_iterations doit etre superieur a 1,"
@@ -30,10 +131,10 @@ void jacobiMethod(double **A, int rowsA, int colsA, double *B, int maxIteration,
             printf("...y compris le vecteur 0(%d)\n", colsA);
         }
     }
-    if (colsA < rowsA) {
-        gaussMethod(A, rowsA, colsA, B, 1);
+    if (colsA < rowsA) { // not a square matrix, but we may be able to save it
+        gaussMethod(A, rowsA, colsA, B, 1); //// at worst O(n^3) arithmetic operations
     }
-    int nilRowsCount = nilRows(A, rowsA, colsA, B, homSys);
+    int nilRowsCount = nilRows(A, rowsA, colsA, B, homSys); /// at worst O(n^2) arithmetic operations
     if (nilRowsCount == -1) {
         return;
     }
@@ -72,11 +173,11 @@ void jacobiMethod(double **A, int rowsA, int colsA, double *B, int maxIteration,
                     sum = 0.0;
                     for (int j = 0; j < size; ++j) {
                         if (i != j) {
-                            sum += A[i][j] * X[j];
+                            sum += A[i][j] * X[j];  //// at worst (max_it) * (n^2) -> O(n^3) arithmetic operations
                         }
                     }
                     Xprev[i] = X[i];
-                    X[i] = (1 / A[i][i]) * (B[i] - sum);
+                    X[i] = (1 / A[i][i]) * (B[i] - sum);//// at worst 2((max_it) * (n)) -> O(n^2) arithmetic operations
                     if (isStable && fabs(Xprev[i] - X[i]) > error) {
                         isStable = 0;
                     }
@@ -99,100 +200,6 @@ void jacobiMethod(double **A, int rowsA, int colsA, double *B, int maxIteration,
         }
     }
 
-}
-
-// NB: this destructive function changes the matrices as a side-effect
-void gaussMethod(double **A, int rowsA, int colsA, double *B, int isJacobiHelper) {
-    if (A == NULL || rowsA < 2 || colsA < 2 || B == NULL) {
-        if (rowsA < 2 || colsA < 2) {
-            printf("we will not deal with a matA(m, n) if m or n is below 2\n");
-        }
-        EMPTY_OR_NULL
-        FAIL_OUT
-    }
-
-    printf("=======================\nGauss\n=======================\n");
-
-
-    int homSys = isHomSys(B, rowsA);
-
-    SHOWME
-    double factor;
-    for (int i = 0; i < rowsA; i++) {
-        int maxPos = i;
-        double maxVal = fabs(A[maxPos][i]);
-
-        for (int k = i + 1; k < rowsA; ++k) {
-            if (fabs(A[k][i]) - maxVal > EPSILON) {
-                maxVal = fabs(A[k][i]);
-                maxPos = k;
-            }
-        }
-
-        if (maxPos != i) {
-            printf("L%d <-> L%d\n", i + 1, maxPos + 1);
-            rowSwap(A, B, i, maxPos, colsA);
-            SHOWME
-        }
-
-        for (int k = i + 1; k < rowsA; ++k) {
-            if (fabs(A[k][i]) < EPSILON || fabs(A[i][i]) < EPSILON) {
-                //to make sure we don't handle numbers too small, for stability
-                continue;
-            } else {
-                factor = (A[k][i] / A[i][i]);
-                printf("L%d <- L%d - (%+06.1f * L%d)\n", k + 1, k + 1, factor, i + 1);
-                rowTransform(A[k], colsA, A[i], factor);
-                B[k] -= factor * B[i];
-                SHOWME
-            }
-        }
-    }
-    if (!isJacobiHelper) {
-        solveGauss(A, rowsA, colsA, B, homSys);
-    }
-}
-
-void solveGauss(double **A, int rowsA, int colsA, double *B, int homSys) {
-    if (A == NULL || B == NULL || rowsA <= 0 || colsA <= 0) {
-        printf("How on Earth?!\n");
-        EMPTY_OR_NULL
-        FAIL_OUT
-    }
-    int nilRowsCount = nilRows(A, rowsA, colsA, B, homSys);
-    if (nilRowsCount == -1) {
-        return;
-    }
-
-    if (rowsA - nilRowsCount == colsA) { // deal with unique solution
-        printf("A * X = B a une solution\n");
-
-        if (homSys) {
-            printf("...le vecteur 0(%d)\n", colsA);
-        } else {
-            double *X = mkColVec(colsA);
-            X[colsA - 1] = B[colsA - 1] / A[colsA - 1][colsA - 1];
-            for (int i = colsA - 2; i >= 0; i--) { //we've already dealt with i = colsA - 1
-                double sum = 0;
-                for (int j = i + 1; j < colsA; j++) {
-                    sum += A[i][j] * X[j];
-                }
-                X[i] = (B[i] - sum) / A[i][i];
-            }
-            printf("X = \n");
-            showCol(X, colsA);
-            free(X);
-        }
-    } else if (rowsA - nilRowsCount < colsA) { // "deal" with infinite amount of solutions
-        // could probably just be phrased as "else"
-        printf("A * X = B a une infinite de solutions\n");
-        if (homSys) {
-            printf("...y compris le vecteur 0(%d)\n", colsA);
-        }
-    } else {
-        printf("we shouldn't be here... no solutions, right?\n");
-        DEBUG
-    }
 }
 
 #endif //Y2_NUM_ALGO_LIN_ALG_H
