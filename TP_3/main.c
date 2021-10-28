92b96cdd3f9cdcfb47bc23b0bd6b535a57e67794
Alexis DRAI <drai.alexis@gmail.com>
2021-10-28 12:47:47 +0200

    Create TP3 structure

---
+++ TP_3/main.c
@@ -0,0 +1,9 @@
+//
+// Created by draia on 28/10/2021.
+//
+#include <stdio.h>
+
+int main(){
+    printf("welcome\n");
+    return 0;
+}
---
+++ TP_3/toolbox.h
@@ -0,0 +1,8 @@
+//
+// Created by draia on 28/10/2021.
+//
+
+#ifndef Y2_NUM_ALGO_TOOLBOX_H
+#define Y2_NUM_ALGO_TOOLBOX_H
+
+#endif //Y2_NUM_ALGO_TOOLBOX_H
