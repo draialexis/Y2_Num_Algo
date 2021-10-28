9d70fa0f7022d2761fbb90bd2aaaba730d4c4670
Alexis DRAI <drai.alexis@gmail.com>
2021-10-28 15:58:55 +0200

    Add python scripting feature, test regression on first set of data

--- TP_3/main.c
+++ TP_3/main.c
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
+#define BUFFER_SIZE 32768
 #define EPSILON 0.00000000000000022204460492503131
 #define DEBUG printf("file %s; line %d\n", __FILE__, __LINE__);
 #define FAIL_OUT exit(EXIT_FAILURE);
@@ -21,15 +23,77 @@ int main() {
     int n;
     coord *arr;
 
-    n = 3;
+//    char str[20];
+//    char *endPtr;
+//    printf("pour quitter ce programme, entrez 'q' en reponse a une requete\n");
+//    printf("combien de coordonnees voulez-vous entrer?\n>");
+//    scanf("%s", str);
+//    n = strtol(str, &endPtr, 10);
+//    cleanCheck(*endPtr);
+//    arr = mkCoordArr(n);
+//    printf("\n");
+//    fill_X_Y(arr, n);
+//    showCoordArr(arr, n);
+//    printf("%d points, complexite: ____, nb d'ops attendu: %d\n",
+//           n, 0);//TODO update
+//    if (leastSqu(arr, n, &a, &b)) {
+//        printf("y = %f x + %f\n", a, b);
+//    } else {
+//        printf("an error occurred\n");
+//    }
+
+
+//    3.2 S1
+    n = 11;
     arr = mkCoordArr(n);
-    arr[0] = (coord) {3,7};
-    arr[1] = (coord) {4, 9};
-    arr[2] = (coord) {5, 8};
-
-    leastSqu(arr, n, &a, &b);
-
-    printf("y = %f x + %f\n", a, b);
+    arr[0] = (coord) {10, 9.14};
+    arr[1] = (coord) {8, 8.14};
+    arr[2] = (coord) {13, 8.74};
+    arr[3] = (coord) {9, 8.77};
+    arr[4] = (coord) {11, 9.26};
+    arr[5] = (coord) {14, 8.1};
+    arr[6] = (coord) {6, 6.13};
+    arr[7] = (coord) {4, 3.1};
+    arr[8] = (coord) {12, 9.13};
+    arr[9] = (coord) {7, 7.26};
+    arr[10] = (coord) {5, 4.74};
+    showCoordArr(arr, n);
+    if (leastSqu(arr, n, &a, &b)) {
+        printf("y = %f x + %f\n", a, b);
+        askPy(arr, a, b, n, 'r');
+    } else {
+        printf("an error occurred\n");
+    }
+//
+//    3.2 S2
+//    n = 11;
+//    arr = mkCoordArr(n);
+//    arr[0] = (coord) {10, 7.46};
+//    arr[1] = (coord) {8, 6.77};
+//    arr[2] = (coord) {13, 12.74};
+//    arr[3] = (coord) {9, 7.11};
+//    arr[4] = (coord) {11, 7.81};
+//    arr[5] = (coord) {14, 8.84};
+//    arr[6] = (coord) {6, 6.08};
+//    arr[7] = (coord) {4, 5.39};
+//    arr[8] = (coord) {12, 8.15};
+//    arr[9] = (coord) {7, 6.42};
+//    arr[10] = (coord) {5, 5.73};
+//
+//    3.2 S3
+//    n = 11;
+//    arr = mkCoordArr(n);
+//    arr[0] = (coord) {8, 6.58};
+//    arr[1] = (coord) {8, 5.76};
+//    arr[2] = (coord) {8, 7.71};
+//    arr[3] = (coord) {8, 8.84};
+//    arr[4] = (coord) {8, 8.47};
+//    arr[5] = (coord) {8, 7.04};
+//    arr[6] = (coord) {8, 5.25};
+//    arr[7] = (coord) {19, 12.5};
+//    arr[8] = (coord) {8, 5.56};
+//    arr[9] = (coord) {8, 7.91};
+//    arr[10] = (coord) {8, 6.89};
 
     return 0;
 }
--- TP_3/toolbox.h
+++ TP_3/toolbox.h
@@ -10,18 +10,158 @@ typedef struct Coordinate {
     double y;
 } coord;
 
-coord *mkCoordArr(int points);
+coord *mkCoordArr(int n);
 
-coord *mkCoordArr(int points) {
-    if (points > 0) {
+void fill_X_Y(coord *arr, int n);
+
+void cleanCheck(char input);
+
+void showCoordArr(coord *arr, int n);
+
+void askPy(const coord *coords, double a, double b, int n, char mthd);
+
+void writePy(const coord *coords, double a, double b, int n, char mthd);
+
+void writeToFile(char *myStr, char *fname);
+
+void checkFopen(FILE *fileName);
+
+coord *mkCoordArr(int n) {
+    if (n > 0) {
         coord *arr = NULL;
-        arr = (coord *) malloc(sizeof(coord) * points);
+        arr = (coord *) malloc(sizeof(coord) * n);
         if (arr == NULL) {
             MALLOC_FAIL
         }
         return arr;
     } else {
         EMPTY_OR_NULL
+        FAIL_OUT
+    }
+}
+
+void fill_X_Y(coord *arr, int n) {
+    if (arr != NULL && n > 0) {
+        char str[20];
+        char *endPtr;
+        coord input;
+        for (int i = 0; i < n; i++) {
+            printf("x_%d <- ", i + 1);
+            scanf("%s", str);
+            input.x = strtod(str, &endPtr);
+            cleanCheck(*endPtr);
+            printf("f(x_%d) <- ", i + 1);
+            scanf("%s", str);
+            input.y = strtod(str, &endPtr);
+            cleanCheck(*endPtr);
+            arr[i] = input;
+        }
+    } else {
+        EMPTY_OR_NULL
+        FAIL_OUT
+    }
+}
+
+void cleanCheck(char input) {
+    getchar();
+    if (input == 'q') exit(0);
+    fflush(stdin);
+}
+
+void showCoordArr(coord *arr, int n) {
+    printf("coordonnees (x, y):\n");
+    if (n > 0 && arr != NULL) {
+        for (int i = 0; i < n; i++) {
+            printf("|%+010.5f, %+010.5f|\n", arr[i].x, arr[i].y);
+        }
+        printf("\n");
+    } else {
+        EMPTY_OR_NULL
+    }
+}
+
+void askPy(const coord *coords, double a, double b, int n, char mthd) {
+    char genPy;
+    printf("\ngenerer un script .py?"
+           "\n* 'o' : oui"
+           "\n* 'n' : non"
+           "\n>");
+    genPy = (char) getchar();
+    cleanCheck(genPy);
+    if (genPy == 'o') {
+        writePy(coords, a, b, n, mthd);
+    }
+}
+
+void writePy(const coord *coords, double a, double b, int n, char mthd) {
+    int bfr = 32;
+    char *fname;
+    char *eqStr = (char *) malloc(sizeof(char) * bfr);
+    sprintf(eqStr, "%c", '\0');
+
+    if (mthd == 'r') {
+        fname = "regression.py";
+        sprintf(eqStr, "'y = %.15f * x %+.15f'", a, b);
+    } else if (mthd == 'e') {
+        fname = "exp_adjust.py";
+    } else if (mthd == 'p') {
+        fname = "pow_adjust.py";
+    }
+
+    char *xStr = (char *) malloc(sizeof(char) * n * bfr);
+    char *yStr = (char *) malloc(sizeof(char) * n * bfr);
+    char *xPart = (char *) malloc(sizeof(char) * bfr);
+    char *yPart = (char *) malloc(sizeof(char) * bfr);
+    sprintf(xStr, "%c", '\0');
+    sprintf(yStr, "%c", '\0');
+    sprintf(xPart, "%c", '\0');
+    sprintf(yPart, "%c", '\0');
+    for (int i = 0; i < n; i++) {
+        sprintf(xPart, "%.15f, ", coords[i].x);
+        sprintf(yPart, "%.15f, ", coords[i].y);
+        strncat(xStr, xPart, bfr);
+        strncat(yStr, yPart, bfr);
+    }
+    char *pyStr = (char *) malloc(sizeof(char) * BUFFER_SIZE);
+    sprintf(pyStr,
+            "import numpy as np\n"
+            "import matplotlib.pyplot as plt\n"
+            "def line_eq(x):\n"
+            "    return %.15f * x %+.15f\n"
+            "x = [%s]\n"
+            "y = [%s]\n"
+            "new_x = []\n"
+            "new_y = []\n"
+            "lo = int(min(x) - 2.0)\n"
+            "hi = int(max(x) + 2.0) + 1\n"
+            "for i in np.linspace(lo, hi, ((hi - lo) * 10)):\n"
+            "    new_x.append(i)\n"
+            "    new_y.append(line_eq(i))\n"
+            "plt.axhline(0, color='grey')\n"
+            "plt.axvline(0, color='grey')\n"
+            "plt.plot(new_x, new_y, color='g', label='regression')\n"
+            "plt.scatter(x, y, 50, color='b', label='donnees')\n"
+            "plt.xlabel('x')\n"
+            "plt.ylabel('y')\n"
+            "plt.title(%s)\n"
+            "plt.legend()\n"
+            "plt.show()\n",
+            a, b, xStr, yStr, eqStr);
+    writeToFile(pyStr, fname);
+}
+
+void writeToFile(char *myStr, char *fname) {
+    FILE *fp = fopen(fname, "w");
+    checkFopen(fp);
+    fputs(myStr, fp);
+    fclose(fp);
+}
+
+
+void checkFopen(FILE *fileName) {
+    if (fileName == NULL) {
+        EMPTY_OR_NULL
+        perror("error while opening file");
         FAIL_OUT
     }
 }
